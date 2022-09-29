6af78baa492b6326c8e38f648b26edc69508a522
Valeria-Aguirre-Uzquiano <valeria.aguirre.uzquiano@gmail.com>
2022-09-28 22:49:15 -0400

    add modal form to principal page

--- src/app/Components/Records/records-logs/records-logs.component.ts
+++ src/app/Components/Records/records-logs/records-logs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-records-logs',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./records-logs.component.css']
 })
 export class RecordsLogsComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private modal: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openCentrado(contenido: any) {
+    this.modal.open(contenido, { centered: true });
+  }
+
+  openScroll(contenido: any) {
+    this.modal.open(contenido, { scrollable: true });
+  }
+
+  tipo = ['Hardware', 'Software', 'Telecom', 'HelpDesk', 'Seguridad', 'Otros'];
+  servicio = ['Mantenimiento', 'Soporte', 'Reparacion', 'Evento', 'Otros'];
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  // TODO: Remove this when we're done
+  // get diagnostic() {
+  //   return JSON.stringify(this.model);
+  // }
+
+  showFormControls(form: any) {
+    return form && form.controls['name'] && form.controls['name'].value; // Dr. IQ
   }
 
 }
